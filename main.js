a081599626247ac138f4f6f81ca2e4ea7f7e6eb2
developer <kill2survive@yandex.ru>
2021-04-25 08:56:08 +0500

    рабочий скрипт

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
+const fs = require('fs')
 const puppeteer = require("puppeteer");
-// we're using async/await - so we need an async function, that we can run
-const run = async(path, url) => {
-    // open the browser and prepare a page
+const { imgDiff } = require("img-diff-js");
+
+const takeScreenshot = async(path, url) => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
-    // set the size of the viewport, so our screenshot will have the desired size
     await page.setViewport({
         width: 1920,
         height: 1080
@@ -16,8 +16,6 @@ const run = async(path, url) => {
         path: path,
         fullPage: true
     })
-
-    // close the browser 
     await browser.close();
 };
 
@@ -25,7 +23,7 @@ function getDateString() {
     var d = new Date()
     let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
     let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
-    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+    let da = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(d);
     let ho = new Intl.DateTimeFormat('en', { hour: 'numeric' }).format(d);
     let mi = new Intl.DateTimeFormat('en', { minute: '2-digit' }).format(d);
     let se = new Intl.DateTimeFormat('en', { second: '2-digit' }).format(d);
@@ -33,8 +31,15 @@ function getDateString() {
     return `${da}-${mo}-${ye}-${ho}-${mi}-${se}`
 }
 
+function delay(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
-async function main(url, interval, screenshotsCount) {
+var diffLogger = fs.createWriteStream('screenshots/diff_info.txt', {
+    flags: 'a'
+})
+
+async function processLoop(url, interval, screenshotsCount, output) {
     if (!screenshotsCount) screenshotsCount = 5
     if (!interval) interval = 5 * 1000
     if (!url) {
@@ -42,34 +47,48 @@ async function main(url, interval, screenshotsCount) {
         return
     }
 
+
     var previousScreenshotName = 'screenshots/' + getDateString() + '.png'
-    await run(previousScreenshotName, url);
-    for (let i = 0; i < screenshotsCount - 1; i++) {
-        setTimeout(async() => {
-            var diffScreenshotName = "screenshots/diff" + getDateString() + ".png"
-                // var defaultName = 'screenshots/default.png'
-                // await run(defaultName, url);
-            var currentScreenshotName = 'screenshots/' + getDateString() + '.png'
-            await run(currentScreenshotName, url);
+    await takeScreenshot(previousScreenshotName, url);
+    for (var i = 0; i < screenshotsCount - 1; i++) {
+        await delay(1000 * interval)
+        var diffScreenshotName = "screenshots/diff" + getDateString() + ".png"
+        var currentScreenshotName = 'screenshots/' + getDateString() + '.png'
+        await takeScreenshot(currentScreenshotName, url);
+        await imgDiff({
+            actualFilename: previousScreenshotName,
+            expectedFilename: currentScreenshotName,
+            diffFilename: diffScreenshotName,
+        }).then(result => {
+            diffLogger.write(`${diffScreenshotName};width:${result.width};height:${result.height};imagesAreSame:;${result.imagesAreSame};diffCount:${result.diffCount}\n`)
+            if (result.imagesAreSame) {
+                fs.unlink(currentScreenshotName, (error) => {})
+                fs.unlink(diffScreenshotName, (error) => {})
+            } else {
+                previousScreenshotName = currentScreenshotName
+            }
+        });
+    }
+}
 
-            const { imgDiff } = require("img-diff-js");
 
-            await imgDiff({
-                actualFilename: previousScreenshotName,
-                expectedFilename: currentScreenshotName,
-                diffFilename: diffScreenshotName,
-            }).then(result => {
-                console.log(result);
-                if (result.diffCount == 0) {
-                    console.log("no changes!");
-                    // delete diff and second screen
-                    const fs = require('fs')
-                    fs.unlink(currentScreenshotName, (error) => console.log(error))
-                    fs.unlink(diffScreenshotName, (error) => console.log(error))
-                } else
-                    previousScreenshotName = currentScreenshotName
-            });
-        }, interval * i * 1000);
+async function main(url, interval, screenshotsCount, output) {
+    await processLoop(url, interval, screenshotsCount, output)
+    diffLogger.end();
+    archive(output);
+}
+
+function archive(output) {
+    if (output) {
+        var zipFolder = require('zip-folder');
+
+        zipFolder('screenshots', output, function(err) {
+            if (err) {
+                console.log('oh no!', err);
+            } else {
+                console.log('screenshots folder zipped');
+            }
+        });
     }
 }
 
@@ -77,8 +96,4 @@ const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv
 
-console.log(argv.url)
-console.log(argv.interval)
-console.log(argv.screenshotsCount)
-
-main(argv.url, argv.interval, argv.screenshotsCount)
+main(argv.url, argv.interval, argv.screenshotsCount, argv.output)
